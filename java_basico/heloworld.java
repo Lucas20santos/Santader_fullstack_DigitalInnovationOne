22d0aba2927c74c0687e8f9809d7d3573025512a
Lucas de Souza Santos <lucas210souza@gmail.com>
2021-08-19 16:15:27 -0300

    subindo

--- java_basico/heloworld.java
+++ java_basico/heloworld.java
@@ -10,7 +10,10 @@ public class heloworld {
         int a = 10;
         int b = 90;
         int soma = a + b;
+        int produto = a * b;
 
         System.out.println("soma = "+ soma);
+        System.out.println("Produto = " + produto);
+
     }
 }
