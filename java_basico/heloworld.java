b5e2ec45a6c605d90af632409979227bf4bae9cd
Lucas de Souza Santos <lucas210souza@gmail.com>
2021-08-19 18:45:58 -0300

    mudanças

--- java_basico/heloworld.java
+++ java_basico/heloworld.java
@@ -10,10 +10,14 @@ public class heloworld {
         int a = 10;
         int b = 90;
         int soma = a + b;
+        int subtracao = a - b;
         int produto = a * b;
+        int divisao = a / b;
 
         System.out.println("soma = "+ soma);
+        System.out.println("Subtração = " + subtracao);
         System.out.println("Produto = " + produto);
-
+        System.out.println("Divisão = " + divisao);
+        
     }
 }
