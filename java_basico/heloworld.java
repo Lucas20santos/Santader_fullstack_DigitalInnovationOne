a3ccdcf1d36e7517ed70b68d5cb4cd0fab5b8f05
Lucas de Souza Santos <lucas210souza@gmail.com>
2021-08-19 16:12:35 -0300

    subindo

---
+++ java_basico/heloworld.java
@@ -0,0 +1,16 @@
+package java_basico;
+
+/**
+ * heloworld
+ */
+public class heloworld {
+
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 90;
+        int soma = a + b;
+
+        System.out.println("soma = "+ soma);
+    }
+}
